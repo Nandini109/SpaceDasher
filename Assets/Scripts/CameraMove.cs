2e20fdef179fd62ab417b1f35c8d75c9f8ce0d2a
Eni <nandiniparmar7781@gmail.com>
2024-09-20 04:47:00 +0530

    Camera Added and Tested

--- Assets/Scripts/CameraMove.cs
+++ Assets/Scripts/CameraMove.cs
@@ -1,3 +1,4 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,9 +6,23 @@ using UnityEngine;
 public class CameraMove : MonoBehaviour
 {
 
-    public float MoveSpeed = 4f;
+    [SerializeField] private float MoveSpeed = 4f;
+
+    [SerializeField] private CinemachineVirtualCamera virtualCamera;
+    [SerializeField] private Transform player;
+    [SerializeField] private float maxZoomOut = 15f; 
+    [SerializeField] private float standardZoom;
+    [SerializeField] private float zoomSpeed = 5f; 
+    [SerializeField] private float rightScreenBoundary = 5f; 
+
 
     private bool stopCamera = false;
+
+
+    private void Start()
+    {
+        standardZoom = virtualCamera.m_Lens.OrthographicSize; 
+    }
 
     private void Update()
     {
@@ -15,7 +30,24 @@ public class CameraMove : MonoBehaviour
         {
             transform.Translate(Vector3.right * MoveSpeed * Time.deltaTime);
         }
+
+        float playerToCameraDistance = player.position.x - Camera.main.transform.position.x;
+
+       
+        if (playerToCameraDistance > rightScreenBoundary)
+        {
+            
+            float targetZoom = Mathf.Clamp(playerToCameraDistance, standardZoom, maxZoomOut);
+            virtualCamera.m_Lens.OrthographicSize = Mathf.Lerp(virtualCamera.m_Lens.OrthographicSize, targetZoom, Time.deltaTime * zoomSpeed);
+            Debug.Log("OH NO Player is trying to ecscape!!!");
+        }
         
+        else if (virtualCamera.m_Lens.OrthographicSize > standardZoom)
+        {
+            virtualCamera.m_Lens.OrthographicSize = Mathf.Lerp(virtualCamera.m_Lens.OrthographicSize, standardZoom, Time.deltaTime * zoomSpeed);
+            Debug.Log("Zoon In!!!!!!!!");
+        }
+
     }
 
     public void StopCamera()
